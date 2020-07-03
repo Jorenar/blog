02fd72096fcc475f4a7b0b55e225e55cd4b4aa57
Jorengarenar <jorengarenar@protonmail.com>
2020-07-03 06:38:39 +0200

    Favicon

--- assets/scripts/win_onload.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  document.querySelector("header .trigger").insertAdjacentElement('afterbegin', s);
-
-  document.querySelector("html").dataset.theme = x;
-  if (document.querySelector("html").dataset.theme == "light") {
-    document.querySelector("#toggleTheme input").checked = true;
-  }
-}
